9b0acd300e813a6d567ceeeee18cab3a2c4b3635
Danilo SOARES <fdanilosoares@gmail.com>
2020-04-04 22:27:39 -0300

    secund_commit

--- controle_gastos/settings.py
+++ controle_gastos/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'controle_gastos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
@@ -156,3 +156,7 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
