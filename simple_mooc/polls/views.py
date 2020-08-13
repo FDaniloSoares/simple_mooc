286f9989307d5062038a9b2b0cb961176d8aafb0
Danilo Soares <fdanilosoares@gmail.com>
2020-08-13 16:52:11 -0300

    add webhook

--- simple_mooc/polls/views.py
+++ simple_mooc/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
 
 # Create your views here.
 def home(request):
@@ -7,4 +9,10 @@ def home(request):
     return render(request, 'home.html')
 
 def contact(request):
-    return render(request, 'contact.html') 
+    return render(request, 'contact.html')
+
+from django.views import View
+
+@csrf_exempt
+def webhook(request):
+    return HttpResponse('pong')
