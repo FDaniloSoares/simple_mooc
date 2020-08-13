5140cd054064f1e58314aef124fdcaed060e2b5a
Danilo Soares <fdanilosoares@gmail.com>
2020-08-13 16:52:11 -0300

    add webhook

--- simple_mooc/polls/urls.py
+++ simple_mooc/polls/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contato/', views.contact, name='contact'),
+    path('webhook/', views.webhook, name='webhook'),
 ]
--- simple_mooc/polls/views.py
+++ simple_mooc/polls/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+import requests
+from ipaddress import ip_address, ip_network
 
 # Create your views here.
 def home(request):
@@ -7,4 +11,18 @@ def home(request):
     return render(request, 'home.html')
 
 def contact(request):
-    return render(request, 'contact.html') 
+    return render(request, 'contact.html')
+
+@csrf_exempt
+def webhook(request):
+    #verify if request came from github
+    forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
+    client_ip_address = ip_address(forwarded_for)
+    whitelist = requests.get('https://api.github.com/meta').json()['hooks']
+
+    for valid_ip in whitelist:
+        if client_ip_address in ip_network(valid_ip):
+            break
+        else:
+            return HttpResponseForbidden('Permission denied.')
+    return HttpResponse('WebHook Works!!!')
